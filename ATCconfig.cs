fcc336c685dcc56e2a407f08500950f9effc1c50
Adonnay76 <adonnay@gmx.de>
2020-04-08 11:53:05 +0200

    First Release

---
+++ ATCconfig.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.IO;
+using TaleWorlds.Core;
+
+namespace AdonnaysTroopChanger.XMLReader
+{
+    public class ATCconfig
+    {
+        private static ATCconfig _instance = null;
+        public static List<TroopConfig> troopConfig = new List<TroopConfig>();
+        public static ATCconfig Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new ATCconfig();
+                    if (_instance == null)
+                        throw new Exception("Unable to find settings in Loader");
+                }
+                return _instance;
+            }
+        }
+
+        public void LoadXML(string xmlpath = "")
+        {
+            if (xmlpath != "")
+            {
+                XmlDocument doc = new XmlDocument();
+
+                doc.Load(xmlpath);
+                //XmlNode xmlNodes = xmlDocument.SelectSingleNode("ATCTroops");
+                XmlElement root = doc.DocumentElement;
+
+                foreach (XmlElement e in root.ChildNodes)
+                {
+                    switch (e.Name)
+                    {
+                        case "source_troop":
+
+                            TroopConfig sourceTroop = new TroopConfig
+                            {
+                                SourceID = e.GetAttribute("id"),
+                                targetTroops = new List<TargetTroop>()
+                            };
+
+                            troopConfig.Add(sourceTroop);
+
+                            foreach (XmlElement ec in e.ChildNodes)
+                            {
+                                sourceTroop.targetTroops.Add(new TargetTroop() { TroopID = ec.GetAttribute("id"), TroopPercent = Convert.ToInt32(ec.GetAttribute("percent")), PlayerOnly = Convert.ToBoolean(ec.GetAttribute("playeronly")) });
+                            }
+                            break;
+                    }
+                }
+            }
+        }
+    }
+
+
+
+    public class TroopConfig
+    {
+        public string SourceID { get; internal set; }
+        public List<TargetTroop> targetTroops;
+    }
+
+    public class TargetTroop
+    {
+        public string TroopID { get; set; }
+        public int TroopPercent { get; set; }
+        public bool PlayerOnly { get; set; }
+    }
+}
---
+++ AdonnaysTroopChangerMain.cs
@@ -0,0 +1,140 @@
+﻿using HarmonyLib;
+using System;
+using System.Linq;
+using System.IO;
+using TaleWorlds.CampaignSystem;
+using TaleWorlds.CampaignSystem.Actions;
+using TaleWorlds.Core;
+using TaleWorlds.Library;
+using TaleWorlds.MountAndBlade;
+using AdonnaysTroopChanger.XMLReader;
+
+namespace AdonnaysTroopChanger
+{
+
+    public class AdonnaysTroopChangerMain : MBSubModuleBase
+    {
+        //public XmlSerializer serializer = new XmlSerializer(typeof(ATCTroops));
+        public static Random rng = new Random();
+
+        protected override void OnSubModuleLoad()
+        {
+            //Harmony.DEBUG = true;
+            Harmony h = new Harmony("de.adonnay.troopchanger");
+            h.PatchAll();
+
+            string configfile = String.Concat(BasePath.Name, "Modules/AdonnaysTroopChanger/Config/ATC.config.xml");
+
+            if (File.Exists(configfile))
+            {
+                ATCconfig.Instance.LoadXML(configfile);
+            }
+
+        }
+
+    }
+}
+
+
+namespace AdonnaysTroopChanger.Patches
+{
+    [HarmonyPatch(typeof(CultureObject), "BasicTroop", MethodType.Getter)]
+    public class PatchBasicTroop
+    {
+        static void Postfix(ref CharacterObject __result)
+        {
+            int rngvalue;
+            foreach (TroopConfig tc in ATCconfig.troopConfig)
+            {
+                if (__result.IsBasicTroop & __result.StringId == tc.SourceID)
+                {
+                    rngvalue = AdonnaysTroopChangerMain.rng.Next(0, 100);
+                    int prevPercent = 0;
+                    foreach (TargetTroop tt in tc.targetTroops)
+                    {
+                        if (rngvalue < (tt.TroopPercent + prevPercent))
+                        {
+                            CharacterObject newTroop = CharacterObject.Find(tt.TroopID);
+                            if (newTroop != null) 
+                            { 
+                                __result = newTroop;
+                                InformationManager.DisplayMessage(new InformationMessage("Volunteer Patched to: " + __result.StringId));
+                                break; //target troop found, we can exit
+                            }
+                            else
+                            {
+                                InformationManager.DisplayMessage(new InformationMessage(tt.TroopID + " invalid!"));
+                            }
+                        }
+                        prevPercent = tt.TroopPercent;
+                    }
+
+                    break; //target troop found, we can exit
+                }
+            }
+        }
+    }
+
+    //[HarmonyPatch(typeof(RecruitVolunteerVM), "OnRecruitMoveToCart")]
+    //public class PatchRecruitVolunteerTroop
+    //{
+    //    static void Prefix(ref RecruitVolunteerTroopVM troop)
+    //    {
+    //        InformationManager.DisplayMessage(new InformationMessage("RecruitAction"));
+    //    }
+    //}
+
+
+    [HarmonyPatch(typeof(RecruitAction), "GetRecruitVolunteerFromIndividual")]
+    public class PatchRecruitActionFromIndividual
+    {
+        static void Prefix(ref MobileParty side1Party, ref CharacterObject subject, ref Hero individual, int bitCode)
+        {
+            //InformationManager.DisplayMessage(new InformationMessage("RecruitAction"));
+            foreach (TroopConfig tc in ATCconfig.troopConfig)
+            {
+                foreach (TargetTroop tt in tc.targetTroops.Where(tt => tt.PlayerOnly == true))
+                {
+                    if (Helpers.CharacterHelper.FindUpgradeRootOf(subject).StringId == tt.TroopID)
+                    {
+                        CharacterObject basicTroop = new CharacterObject();
+                        basicTroop = CharacterObject.Find(tc.SourceID);
+                        if (basicTroop != null)
+                        {
+                            InformationManager.DisplayMessage(new InformationMessage(basicTroop.StringId + " recruited instead of " + subject.StringId));
+
+                            individual.VolunteerTypes[bitCode] = subject = basicTroop;
+                            break;
+                            }
+                        else
+                        {
+                            InformationManager.DisplayMessage(new InformationMessage(tt.TroopID + " invalid!"));
+                        }
+                    }
+                }
+            }
+        }
+
+        //static void Postfix(ref MobileParty side1Party, ref CharacterObject subject, ref Hero individual, int bitCode)
+        //{
+        //    //InformationManager.DisplayMessage(new InformationMessage("RecruitAction"));
+        //}
+    }
+
+
+    //[HarmonyPatch(typeof(RecruitAction), "GetRecruitVolunteerFromIndividualToGarrison")]
+    //public class PatchRecruitActionToGarrison
+    //{
+    //    static void Prefix(ref MobileParty side1Party, ref CharacterObject subject, ref Hero individual, int bitCode)
+    //    {
+    //        InformationManager.DisplayMessage(new InformationMessage("RecruitAction"));
+    //    }
+
+    //    static void Postfix(ref MobileParty side1Party, ref CharacterObject subject, ref Hero individual, int bitCode)
+    //    {
+    //        InformationManager.DisplayMessage(new InformationMessage("RecruitAction"));
+    //    }
+    //}
+
+}
+
